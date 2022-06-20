51af2028fd2a8358f3c1d1f8dd255c6d4b259364
qwespong <99456611+qwespong@users.noreply.github.com>
2022-06-20 10:38:58 +0100

    added test for user models

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,35 @@
+"""
+Models for db
+"""
+
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin)
 import psycopg2
 
+
 # Create your models here.
+class UserManager(BaseUserManager):
+    """To manage user in the db"""
+    def create_user(self, email, password=None, **extra_fields):
+        """Create save and return new user"""
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user 
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+
+    USERNAME_FIELD = 'email'
